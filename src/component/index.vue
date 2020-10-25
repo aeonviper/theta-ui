eb0f7c5183402954f5004d33b3ebfcff42cee8b3
aeonviper <viperblaster@gmail.com>
2020-10-25 19:27:10 +0700

    Project

---
+++ src/component/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<v-app>
+		<v-main style="display:flex;align-items:center;">
+			<center>
+				<img src="../asset/logo.png" />
+			</center>
+		</v-main>
+	</v-app>
+</template>
---
+++ src/component/notFound.vue
@@ -0,0 +1,11 @@
+<template>
+	<v-app>
+		<v-main style="display:flex;align-items:center;">
+			<center>
+				<h1>Page not found</h1>
+				<p>The page you are looking for does not exist</p>
+				<v-btn small @click="$router.back()" color="primary">Go back</v-btn>
+			</center>
+		</v-main>
+	</v-app>
+</template>
