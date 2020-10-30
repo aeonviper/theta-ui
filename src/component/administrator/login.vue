be2dbb7e0e94b24544334b1877ecbb225e0f49e1
aeonviper <viperblaster@gmail.com>
2020-10-30 20:59:40 +0700

    Refactor

--- src/component/administrator/login.vue
+++ src/component/administrator/login.vue
@@ -12,7 +12,7 @@
 					</v-card-text>
 					<v-card-actions>
 						<v-spacer></v-spacer>
-						<v-btn @click="login" color="primary">Login</v-btn>
+						<v-btn @click="login()" color="primary">Login</v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-form>
