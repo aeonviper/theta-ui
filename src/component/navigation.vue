5b9849d94ec7ac163d123998210d9075797411e9
aeonviper <viperblaster@gmail.com>
2021-08-19 06:33:17 +0700

    Notification use v-html

--- src/component/navigation.vue
+++ src/component/navigation.vue
@@ -11,12 +11,12 @@
 					</v-btn>
 				</div>
 				<template v-if="$store.state.notification.content && $store.state.notification.content.type === 'NOTIFICATION'">
-					<div style="color:#C42126;font-size:14px;" :key="item" v-for="item in $store.state.notification.content.fieldErrorList">{{item}}</div>
-					<div style="color:#ED5025;font-size:14px;" :key="item" v-for="item in $store.state.notification.content.errorList">{{item}}</div>
-					<div style="color:#E26026;font-size:14px;" :key="item" v-for="item in $store.state.notification.content.noticeList">{{item}}</div>
+					<div style="color:#C42126;font-size:14px;" :key="item" v-for="item in $store.state.notification.content.fieldErrorList" v-html="item"></div>
+					<div style="color:#ED5025;font-size:14px;" :key="item" v-for="item in $store.state.notification.content.errorList" v-html="item"></div>
+					<div style="color:#E26026;font-size:14px;" :key="item" v-for="item in $store.state.notification.content.noticeList" v-html="item"></div>
 				</template>
 				<template v-else>
-					<div style="color:#E26026;font-size:14px;">{{$store.state.notification.content}}</div>
+					<div style="color:#E26026;font-size:14px;" v-html="$store.state.notification.content"></div>
 				</template>
 				<div style="text-align:center;margin-top:20px;">
 					<v-btn color="primary" @click="hideNotification">Close</v-btn>
